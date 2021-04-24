4e3899189c073827a1304c01ca7a534371552351
Kenneth Kaye <kenneth.kaye@gmail.com>
2021-04-24 10:34:25 -0600

    Initial commit

---
+++ main.ts
@@ -0,0 +1,16 @@
+const testObj = {
+  pilot: ["Han Solo", "Lando Calrissian"],
+  copilot: ["Chewbacca", "Rey"],
+  ship: "Falcon",
+  speed: "1.5c"
+}
+
+function generatePermutations (currObj : Object): Object[] {
+  var resultsArr : Object[];
+  //stop recursion when the current object is empty
+  if (Object.keys(currObj).length == 0) {
+    //stop processing - nothing here
+  }
+
+  return resultsArr;
+};
